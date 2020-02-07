259e552e0a6bb80ba23ac349466619d42d6aa9c1
91703 <91703@LAPTOP-DHG1FKQ3>
2020-02-07 21:03:03 +0530

    Completed.

---
+++ Task_2/src/test/java/EpamWeek_2/Task_2/Abstract_Method.java
@@ -0,0 +1,6 @@
+package EpamWeek_2.Task_2;
+public abstract class Abstract_Method {
+	public abstract int total_weight(int a[],int n);
+	public abstract int num_of_candy(int low,int high,int a[],int n);
+
+}
