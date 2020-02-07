1a24b8557259e4040c8c278fc14cd0a82075b187
91703 <91703@LAPTOP-DHG1FKQ3>
2020-02-07 21:42:39 +0530

    After test removal

--- Task_2/src/main/java/EpamWeek_2/Task_2/App.java
+++ Task_2/src/main/java/EpamWeek_2/Task_2/App.java
@@ -1,13 +1,6 @@
 package EpamWeek_2.Task_2;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
--- Task_2/src/test/java/EpamWeek_2/Task_2/App.java
+++
@@ -1,6 +0,0 @@
-package EpamWeek_2.Task_2;
-public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-    }
-}
